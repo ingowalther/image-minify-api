d91195c790dd714be8efb61246373b3a119f524f
Serghei Luchianenco <serghei.luchianenco@freshcells.de>
2016-10-11 00:00:58 +0200

    Compressor and Console commands registration with tagged services

---
+++ src/IngoWalther/ImageMinifyApi/CompilerPass/CommandPass.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Serghei Luchianenco (s@luchianenco.com)
+ * Date: 10/10/16
+ * Time: 23:20
+ */
+namespace IngoWalther\ImageMinifyApi\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class CommandPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has('console')) {
+            return;
+        }
+
+        $definition = $container->findDefinition('console');
+        $taggedServices = $container->findTaggedServiceIds('console.command');
+
+        foreach ($taggedServices as $id => $tags) {
+            foreach ($tags as $attributes) {
+                $definition->addMethodCall(
+                    'add',
+                    array(new Reference($id))
+                );
+            }
+        }
+    }
+}
